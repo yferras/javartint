fce74ba68e2d43092bc36f23fe79b2602fd5d8c0
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2014-08-23 23:16:07 -0400

    Adicionado el mecanismo para los eventos.

---
+++ crow-jai/crow-jai-core/src/main/java/crow/jai/core/util/AlgorithmEvent.java
@@ -0,0 +1,41 @@
+package crow.jai.core.util;
+
+import java.util.EventObject;
+
+import crow.jai.core.Algorithm;
+import crow.jai.core.Solution;
+
+/**
+ * Algorithm event object.
+ * 
+ * @author Eng. Ferrás Cecilio, Yeinier.
+ * @version 0.0.1
+ */
+public class AlgorithmEvent extends EventObject {
+
+	private static final long serialVersionUID = -4552301165892831022L;
+	private final Solution solution;
+
+        /**
+         * Constructor.
+         * 
+         * @param source 
+         *                  The object on which the Event initially occurred.
+         * @param solution 
+         *                  The solution.
+         */
+	public AlgorithmEvent(Algorithm source, Solution solution) {
+		super(source);
+		this.solution = solution;
+	}
+
+        /**
+         * Gets the solution.
+         * @return 
+         *          The solution.
+         */
+	public Solution getSolution() {
+		return solution;
+	}
+
+}
---
+++ crow-jai/crow-jai-core/src/main/java/crow/jai/core/util/SolutionChangeListener.java
@@ -0,0 +1,20 @@
+package crow.jai.core.util;
+
+import java.util.EventListener;
+
+/**
+ * Listener to know when algorithm has changed the solution.
+ * 
+ * @author Eng. Ferrás Cecilio, Yeinier.
+ * @version 0.0.1
+ */
+public interface SolutionChangeListener extends EventListener {
+
+	/**
+	 * This method should be fired when the algorithm updates the solution.
+	 * 
+	 * @param event
+	 *            algorithm event.
+	 */
+	void solutionUpdated(AlgorithmEvent event);
+}
