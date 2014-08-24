0795c5647367dbf498d219e8769f2bf84f69ae9a
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2014-08-23 23:41:55 -0400

    Agregando javadoc a los métodos.

--- crow-jai/crow-jai-core/src/main/java/crow/jai/core/OptimizationAlgorithm.java
+++ crow-jai/crow-jai-core/src/main/java/crow/jai/core/OptimizationAlgorithm.java
@@ -4,13 +4,21 @@ import crow.jai.core.util.Optimize;
 
 /**
  * This interface allows to create algorithms to optimize.
- * 
+ *
  * @author Eng. Ferrás Cecilio, Yeinier
  * @version 0.0.1
  */
 public interface OptimizationAlgorithm extends Algorithm {
 
-	public Optimize getOptimize();
+    /**
+     *Gets the optimize type.
+     * @return optimize type.
+     */
+    public Optimize getOptimize();
 
-	public void setOptimize(Optimize optimize);
+    /**
+     * Sets the optimize type.
+     * @param optimize the optimize type.
+     */
+    public void setOptimize(Optimize optimize);
 }
