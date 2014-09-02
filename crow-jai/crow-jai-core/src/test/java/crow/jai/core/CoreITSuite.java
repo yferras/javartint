3a15dd0ce492c4618149523509574035b35038f3
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2014-09-02 15:04:18 -0400

    Eliminados los commentarios iniciales, Actualizado el nombre del Autor.

--- crow-jai/crow-jai-core/src/test/java/crow/jai/core/CoreITSuite.java
+++ crow-jai/crow-jai-core/src/test/java/crow/jai/core/CoreITSuite.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package crow.jai.core;
 
 import org.junit.After;
@@ -15,7 +9,7 @@ import org.junit.runners.Suite;
 
 /**
  *
- * @author yey
+ * @author Eng. Ferrás Cecilio, Yeinier
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({crow.jai.core.AbstractAlgorithmIT.class})
