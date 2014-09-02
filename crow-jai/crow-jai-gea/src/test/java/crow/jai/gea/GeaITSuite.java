e0e225ec184d505c6729fc80c85399d11b26a27e
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2014-09-02 16:16:12 -0400

    Agregando las suits de tests para testear con maven.

---
+++ crow-jai/crow-jai-gea/src/test/java/crow/jai/gea/GeaITSuite.java
@@ -0,0 +1,34 @@
+package crow.jai.gea;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+/**
+ *
+ * @author Eng. Ferrás Cecilio, Yeinier
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({crow.jai.gea.gene.DefaultGeneIT.class, crow.jai.gea.genome.DefaultGenomeIT.class})
+public class GeaITSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
