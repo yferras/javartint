5afbc6bedb201fc96462389bd557798dad1d8781
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2014-11-09 23:06:52 -0500

    Adicionando clase abstracta para los operadores de cruzamiento.

---
+++ crow-jai/crow-jai-gea/src/main/java/crow/jai/gea/function/crossing/AbstractCrossingFunction.java
@@ -0,0 +1,58 @@
+package crow.jai.gea.function.crossing;
+
+import crow.jai.core.util.RandomGenerator;
+import crow.jai.gea.gene.Gene;
+import crow.jai.gea.genome.Genome;
+
+/**
+ * Abstract class that implements interface {@link crow.jai.gea.function.crossing.CrossingFunction}
+ *
+ * @author Eng. Ferrás Cecilio, Yeinier
+ * @version 0.0.1
+ */
+abstract public class AbstractCrossingFunction<T extends Genome<? extends Gene<?>>>
+        implements CrossingFunction<T> {
+
+    private double probability;
+    private RandomGenerator randomGenerator;
+
+    /**
+     * Constructor.
+     *
+     * @param probability probability of crossing
+     * @param randomGenerator random generator
+     */
+    protected AbstractCrossingFunction(double probability, RandomGenerator randomGenerator) {
+        this.probability = probability;
+        this.randomGenerator = randomGenerator;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param probability probability of crossing
+     */
+    protected AbstractCrossingFunction(double probability) {
+        this(probability, new RandomGenerator.SystemDefaultRandomGenerator());
+    }
+
+    @Override
+    public double getCrossingProbability() {
+        return probability;
+    }
+
+    @Override
+    public void setCrossingProbability(final double probability) {
+        this.probability = probability;
+    }
+
+    @Override
+    public RandomGenerator getRandomGenerator() {
+        return randomGenerator;
+    }
+
+    @Override
+    public void setRandomGenerator(final RandomGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+}
