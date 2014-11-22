65b15d9a1cdeffde60ad58b6587d8d4b7d262a1c
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2014-11-22 16:21:40 -0500

    Agregada una especialización para la interface Function

---
+++ crow-jai/crow-jai-core/src/main/java/crow/jai/core/util/function/AbstractProbabilisticFunction.java
@@ -0,0 +1,67 @@
+package crow.jai.core.util.function;
+
+import crow.jai.core.util.RandomGenerator;
+
+/**
+ * Class that implements partially {@link ProbabilisticFunction} to create
+ * generic probabilistic functions.
+ *
+ * @param <R> Type of result
+ * @param <P> Type of parameters
+ * @author Eng. Ferrás Cecilio, Yeinier
+ * @version 0.0.1
+ */
+abstract public class AbstractProbabilisticFunction<R, P>
+        implements ProbabilisticFunction<R, P> {
+
+    private double probability;
+    private RandomGenerator randomGenerator;
+
+    /**
+     * Constructor, initializes instances with the given parameters.
+     *
+     * @param probability     probability of crossover
+     * @param randomGenerator random generator
+     */
+    protected AbstractProbabilisticFunction(double probability,
+                                            RandomGenerator randomGenerator) {
+        this.probability = probability;
+        this.randomGenerator = randomGenerator;
+    }
+
+    /**
+     * Constructor, initializes instances with probability specified by
+     * {@code probability} parameter and random generator is an instance of
+     * {@link crow.jai.core.util.RandomGenerator.SystemDefaultRandomGenerator}.
+     *
+     * @param probability probability of crossover
+     */
+    protected AbstractProbabilisticFunction(double probability) {
+        this(probability, new RandomGenerator.SystemDefaultRandomGenerator());
+    }
+
+    @Override
+    public double getProbability() {
+        return probability;
+    }
+
+    @Override
+    public void setProbability(final double probability) {
+        if (probability < 0 || probability > 1.0) {
+            throw new IllegalArgumentException(
+                    "'probability' must between 0.0 and 1.0");
+        }
+        this.probability = probability;
+    }
+
+    @Override
+    public RandomGenerator getRandomGenerator() {
+        return randomGenerator;
+    }
+
+    @Override
+    public void setRandomGenerator(final RandomGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+
+}
---
+++ crow-jai/crow-jai-core/src/main/java/crow/jai/core/util/function/ProbabilisticFunction.java
@@ -0,0 +1,46 @@
+package crow.jai.core.util.function;
+
+import crow.jai.core.util.RandomGenerator;
+
+/**
+ * Interface to create generic probabilistic functions.
+ *
+ * @param <R> Type of result
+ * @param <P> Type of parameters
+ * @author Eng. Ferrás Cecilio, Yeinier
+ * @version 0.0.1
+ */
+public interface ProbabilisticFunction<R, P> extends Function<R, P> {
+
+    /**
+     * Gets the probability, a number between {@code 0.0} and {@code 0.0}.
+     * @return the probability
+     */
+    double getProbability();
+
+    /**
+     * Sets the probability, a number that must between {@code 0.0} and
+     * {@code 0.0}.
+     *
+     * @param probability the probability
+     *
+     * @throws java.lang.IllegalArgumentException if {@code probability is not
+     * inside 0.0 and 1.0}
+     */
+    void setProbability(final double probability)
+            throws IllegalArgumentException;
+
+    /**
+     * Gets the random generator.
+     * @return the random generator
+     */
+    RandomGenerator getRandomGenerator();
+
+    /**
+     * Sets the random generator.
+     *
+     * @param randomGenerator the random generator
+     */
+    void setRandomGenerator(final RandomGenerator randomGenerator);
+
+}
