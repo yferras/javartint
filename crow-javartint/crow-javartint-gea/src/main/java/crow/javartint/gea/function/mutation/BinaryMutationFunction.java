e9f6fa888c6d536cf817dd2153b558ad47701f1a
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2015-01-10 13:04:32 -0500

    Reimplementada la lógica de esta función.

--- crow-javartint/crow-javartint-gea/src/main/java/crow/javartint/gea/function/mutation/BinaryMutationFunction.java
+++ crow-javartint/crow-javartint-gea/src/main/java/crow/javartint/gea/function/mutation/BinaryMutationFunction.java
@@ -1,10 +1,7 @@
 package crow.javartint.gea.function.mutation;
 
-import crow.javartint.gea.gene.Gene;
+import crow.javartint.gea.gene.IntegerArrayGene;
 import crow.javartint.gea.genome.Genome;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Class that represents an binary mutation function.
@@ -13,27 +10,23 @@ import java.util.List;
  * @author Eng. Ferrás Cecilio, Yeinier
  * @version 0.0.1
  */
-public class BinaryMutationFunction<T extends Genome<? extends Gene<Integer>>>
+public class BinaryMutationFunction<T extends Genome<IntegerArrayGene>>
         extends AbstractMutationFunction<T> {
 
     @Override
-    protected void mutate(T subject) {
-        int n = subject.getNumberOfGenes(); // Gets total of genes
-        int mutate = getRandomGenerator().nextInt(n); // generate number of
-        // genes to mutate
-        while (mutate == 0) {
-            // generate different of zero
-            mutate = getRandomGenerator().nextInt(n);
-        }
-        List<Integer> old = new ArrayList<>(mutate);
-        for (int i = 0; i < mutate; i++) {
-            int index = getRandomGenerator().nextInt(n);
-            while (old.contains(index)) {
-                index = getRandomGenerator().nextInt(n);
+    protected T mutate(T subject) {
+        boolean muted = false;
+        while (!muted) {
+            for (IntegerArrayGene gene : subject) {
+                for (int i = 0; i < gene.length(); i++) {
+                    if (getRandomGenerator().nextDouble() < getProbability()){
+                        int val = gene.getAllele(i);
+                        gene.setAllele(i, val == 0 ? 1 : 0);
+                        muted = true;
+                    }
+                }
             }
-            final Gene<Integer> gene = subject.getGene(index);
-            gene.setData(gene.getData() == 0 ? 1 : 0);
-            old.add(index);
         }
+        return subject;
     }
 }
