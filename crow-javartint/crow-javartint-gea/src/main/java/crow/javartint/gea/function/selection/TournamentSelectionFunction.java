52f0bd760338508fca51d0375d137ab20451c008
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2015-02-17 23:31:40 -0500

    Agregado los headers de la licencia GPLv3 a cada fichero java, más la propia licencia a cada proyecto.

--- crow-javartint/crow-javartint-gea/src/main/java/crow/javartint/gea/function/selection/TournamentSelectionFunction.java
+++ crow-javartint/crow-javartint-gea/src/main/java/crow/javartint/gea/function/selection/TournamentSelectionFunction.java
@@ -1,5 +1,27 @@
 package crow.javartint.gea.function.selection;
 
+/*
+ * #%L
+ * Crow JavArtInt GEA
+ * %%
+ * Copyright (C) 2014 - 2015 Eng. Ferrás Cecilio, Yeinier
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import crow.javartint.gea.function.scaling.AbstractScalingMethod;
 import crow.javartint.gea.gene.Gene;
 import crow.javartint.gea.genome.Genome;
@@ -46,15 +68,15 @@ public class TournamentSelectionFunction<T extends Genome<? extends Gene<?>>>
 
     private T tournamentSelection(List<T> genomes) {
         double bestFitnessSoFar = 0;
-        int chosenone = 0;
+        int chosenOne = 0;
         Random rand = new Random();
         for (int i = 0; i < getNumToSelect(); i++) {
             int thisTry = rand.nextInt(genomes.size());
             if (genomes.get(thisTry).getFitness() > bestFitnessSoFar) {
-                chosenone = thisTry;
+                chosenOne = thisTry;
                 bestFitnessSoFar = genomes.get(thisTry).getFitness();
             }
         }
-        return genomes.get(chosenone);
+        return genomes.get(chosenOne);
     }
 }
--- crow-javartint/crow-javartint-gea/src/test/java/crow/javartint/gea/function/mutattion/BinaryMutationFunctionIT.java
+++ crow-javartint/crow-javartint-gea/src/test/java/crow/javartint/gea/function/mutattion/BinaryMutationFunctionIT.java
@@ -1,16 +1,35 @@
 package crow.javartint.gea.function.mutattion;
 
+/*
+ * #%L
+ * Crow JavArtInt GEA
+ * %%
+ * Copyright (C) 2014 - 2015 Eng. Ferrás Cecilio, Yeinier
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import crow.javartint.gea.GenomeConstants;
 import crow.javartint.gea.function.mutation.BinaryMutationFunction;
-import crow.javartint.gea.gene.DefaultGene;
-import crow.javartint.gea.gene.Gene;
 import crow.javartint.gea.gene.IntegerArrayGene;
 import crow.javartint.gea.genome.DefaultGenome;
 import crow.javartint.gea.genome.Genome;
 import org.junit.*;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 /**
  * @author Eng. Ferrás Cecilio, Yeinier
