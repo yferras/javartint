7c4ac38b1876f5a73cf2d161e1875385136093b0
Yeinier Ferrás Cecilio <yeinier.ferras@gmail.com>
2015-02-19 16:34:49 -0500

    Agregadas las dos clases bases para los algoritmos evolutivos y algoritmos genéticos.

---
+++ crow-javartint/crow-javartint-gea/src/main/java/crow/javartint/gea/AbstractEvolutionaryAlgorithm.java
@@ -0,0 +1,245 @@
+package crow.javartint.gea;
+
+/*
+ * #%L
+ * Crow JavArtInt GEA
+ * %%
+ * Copyright (C) 2014 - 2015 Eng. Ferrás Cecilio, Yeinier
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import crow.javartint.core.AbstractAlgorithm;
+import crow.javartint.core.IterativeAlgorithm;
+import crow.javartint.core.OptimizationAlgorithm;
+import crow.javartint.core.util.Optimize;
+import crow.javartint.core.util.function.Function;
+import crow.javartint.gea.function.decoder.DecoderFunction;
+import crow.javartint.gea.function.generator.GeneratorFunction;
+import crow.javartint.gea.gene.Gene;
+import crow.javartint.gea.genome.Genome;
+import crow.javartint.gea.util.GenomeFilter;
+
+import java.util.List;
+
+/**
+ * Abstract class that provides a sets of functionalities to subclassing evolutionary algorithms
+ *
+ * @param <T> Any derived class from {@link crow.javartint.gea.genome.Genome}
+ * @author Eng. Ferrás Cecilio, Yeinier.
+ * @version 0.0.1
+ */
+public abstract class AbstractEvolutionaryAlgorithm<T extends Genome<? extends Gene<?>>>
+        extends AbstractAlgorithm<T>
+        implements OptimizationAlgorithm<T>, IterativeAlgorithm<T> {
+
+    private GenomeFilter<T> genomeFilter;
+
+    private List<T> population;
+
+    private int populationSize;
+
+    private long generations;
+
+    private Optimize optimize;
+
+    private double bestFitnessScore;
+
+    private DecoderFunction<T> decoder;
+
+    private GeneratorFunction<T> generator;
+
+    /**
+     * Initializes this class.
+     *
+     * @param populationSize the population limit
+     * @param optimize       the optimization way
+     * @param decoder        function to decode the genome
+     * @param generator      function to generate genomes
+     */
+    public AbstractEvolutionaryAlgorithm(int populationSize,
+                                         Optimize optimize,
+                                         DecoderFunction<T> decoder,
+                                         GeneratorFunction<T> generator) {
+        this.populationSize = populationSize;
+        this.decoder = decoder;
+        this.generator = generator;
+        setOptimize(optimize);
+    }
+
+    /**
+     * This method must be implemented by subclasses to simulate the evolution process.
+     */
+    public abstract void evolve() throws Exception;
+
+    /**
+     * This method must be implemented to evolve one generation
+     */
+    protected abstract void epoch();
+
+    @SuppressWarnings("unchecked")
+    protected void updateFitnessScores() {
+        for (T genome : getPopulation()) {
+            Double currentFitnessScore = getDecoder().evaluate(genome);
+            genome.setFitness(currentFitnessScore);
+
+            if ((getOptimize() == Optimize.MAX
+                    && currentFitnessScore > getBestFitnessScore())
+                    ^ (getOptimize() == Optimize.MIN
+                    && currentFitnessScore < getBestFitnessScore())) {
+                setBestFitnessScore(currentFitnessScore);
+                try {
+                    setSolution((T) genome.clone());
+                } catch (CloneNotSupportedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Used to create the start population.
+     */
+    protected void createStartPopulation() {
+        while (getPopulationSize() < getPopulation().size()) {
+            T genome = getGenerator().evaluate();
+            if (getGenomeFilter() != null && getGenomeFilter().accept(genome)) {
+                getPopulation().add(genome);
+            } else {
+                getPopulation().add(genome);
+            }
+        }
+    }
+
+    /**
+     * Each time that's method is invoked the {@code generations} attribute increases its value in one.
+     */
+    protected void increaseGenerations() {
+        generations++;
+    }
+
+    @Override
+    public Long getIterations() {
+        return getGenerations();
+    }
+
+    @Override
+    public Optimize getOptimize() {
+        return optimize;
+    }
+
+    @Override
+    final public void setOptimize(Optimize optimize) {
+        switch (optimize) {
+            case MAX:
+                setBestFitnessScore(Double.MIN_VALUE);
+                break;
+            case MIN:
+                setBestFitnessScore(Double.MAX_VALUE);
+                break;
+        }
+        this.optimize = optimize;
+    }
+
+    /**
+     * Gets an instance of {@link crow.javartint.core.util.function.Function} that decodes the the genome.
+     *
+     * @return an instance of {@link crow.javartint.core.util.function.Function}
+     */
+    public Function<Double, T> getDecoder() {
+        return decoder;
+    }
+
+    /**
+     * Gets the value of best fitness score.
+     *
+     * @return the value of best fitness score.
+     */
+    public double getBestFitnessScore() {
+        return bestFitnessScore;
+    }
+
+    /**
+     * Updates the value of best fitness score.
+     *
+     * @param bestFitnessScore new value of best fitness score
+     */
+    protected void setBestFitnessScore(double bestFitnessScore) {
+        this.bestFitnessScore = bestFitnessScore;
+    }
+
+    /**
+     * Retrieves the generations.
+     *
+     * @return generations.
+     */
+    public long getGenerations() {
+        return generations;
+    }
+
+    /**
+     * Gets the population size.
+     *
+     * @return population size.
+     */
+    public int getPopulationSize() {
+        return populationSize;
+    }
+
+    /**
+     * Gets the population.
+     *
+     * @return a list with a population.
+     */
+    protected List<T> getPopulation() {
+        return population;
+    }
+
+    /**
+     * Sets the new population.
+     *
+     * @param population new population list.
+     */
+    protected void setPopulation(List<T> population) {
+        this.population = population;
+    }
+
+    /**
+     * Gets the genome filter.
+     *
+     * @return an instance of {@link crow.javartint.gea.util.GenomeFilter}.
+     */
+    protected GenomeFilter<T> getGenomeFilter() {
+        return genomeFilter;
+    }
+
+    /**
+     * Sets the genome filter
+     *
+     * @param genomeFilter an instance of {@link crow.javartint.gea.util.GenomeFilter}
+     */
+    protected void setGenomeFilter(GenomeFilter<T> genomeFilter) {
+        this.genomeFilter = genomeFilter;
+    }
+
+    /**
+     * Gets the function that generates genome
+     * @return the instance of {@link crow.javartint.gea.function.generator.GeneratorFunction}
+     */
+    public GeneratorFunction<T> getGenerator() {
+        return generator;
+    }
+}
---
+++ crow-javartint/crow-javartint-gea/src/main/java/crow/javartint/gea/AbstractGeneticAlgorithm.java
@@ -0,0 +1,204 @@
+package crow.javartint.gea;
+
+/*
+ * #%L
+ * Crow JavArtInt GEA
+ * %%
+ * Copyright (C) 2014 - 2015 Eng. Ferrás Cecilio, Yeinier
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import crow.javartint.core.util.Optimize;
+import crow.javartint.gea.function.crossover.CrossoverFunction;
+import crow.javartint.gea.function.decoder.DecoderFunction;
+import crow.javartint.gea.function.generator.GeneratorFunction;
+import crow.javartint.gea.function.mutation.MutationFunction;
+import crow.javartint.gea.function.selection.RandomSelectionFunction;
+import crow.javartint.gea.function.selection.SelectionFunction;
+import crow.javartint.gea.gene.Gene;
+import crow.javartint.gea.genome.Genome;
+
+import java.util.*;
+
+/**
+ * Abstract class that provides a sets of functionalities to subclassing genetic algorithms
+ *
+ * @param <T> Any derived class from {@link crow.javartint.gea.genome.Genome}
+ * @author Eng. Ferrás Cecilio, Yeinier.
+ * @version 0.0.1
+ */
+public abstract class AbstractGeneticAlgorithm<T extends Genome<? extends Gene<?>>>
+        extends AbstractEvolutionaryAlgorithm<T> {
+
+    private final CrossoverFunction<T> crossoverFunction;
+
+    private final MutationFunction<T> mutationFunction;
+
+    private final SelectionFunction<T> selectionFunction;
+
+    private SelectionFunction<T> selectionFunctionToParents;
+
+    /**
+     * Initializes this class.
+     * @param populationSize    the population limit
+     * @param optimize          the optimization way
+     * @param decoder           function to decode the genome
+     * @param generator         function to generate genomes
+     * @param crossoverFunction function to crossing process
+     * @param mutationFunction  function to mutation process
+     * @param selectionFunction function to selection process
+     */
+    public AbstractGeneticAlgorithm(int populationSize,
+                                    Optimize optimize,
+                                    DecoderFunction<T> decoder,
+                                    GeneratorFunction<T> generator,
+                                    CrossoverFunction<T> crossoverFunction,
+                                    MutationFunction<T> mutationFunction,
+                                    SelectionFunction<T> selectionFunction) {
+        super(populationSize, optimize, decoder, generator);
+        this.crossoverFunction = crossoverFunction;
+        this.mutationFunction = mutationFunction;
+        this.selectionFunction = selectionFunction;
+        selectionFunctionToParents = new RandomSelectionFunction<>();
+    }
+
+    /**
+     * This method sets {@code true} to {@code running} attribute, and calls
+     * {@link #evolve()} method. Implemented from Runnable interface.
+     */
+    @Override
+    public void run() {
+        beginAlgorithm();
+        evolve();
+        fireAlgorithmFinishedEvent();
+    }
+
+    /**
+     * <p>This method is the responsible for create and evolve of the population.</p>
+     */
+    @Override
+    public void evolve() {
+        createStartPopulation();
+        while (isRunning()) {
+            updateFitnessScores();
+            if (testConstraint()) {
+                stop();
+                break;
+            }
+            epoch();
+            increaseGenerations();
+        }
+    }
+
+    /**
+     * <p>
+     * In this method a new population is created from the selection and
+     * crossing of individuals of the current population. After completing the
+     * number of individuals, the current population is replaced by the new. The
+     * methods are executed in this order:
+     * </p>
+     * <ol>
+     * <li>If {@link #getSelectionFunction()} is not {@code null}, selects
+     * (according selection method) the configured number of genomes to new
+     * population, otherwise continues.</li>
+     * <li>While the size o new population will be less than the size of
+     * current population.</li>
+     * <ol>
+     * <li>Selects the two parents.</li>
+     * <li>Crossing the pre-selected parents.</li>
+     * <li>Mutates the offspring.</li>
+     * <li>If {@link #getGenomeFilter()} is distinct of {@code null},
+     * the new offspring will be filtered,
+     * and only the successfully filtered genomes will be added to the new generation,
+     * otherwise the offspring will be added to the new population.</li>
+     * </ol>
+     * <li>Old population is replaced by new.</li>
+     * </ol>
+     */
+    @Override
+    protected void epoch() {
+        Set<T> newGeneration = new LinkedHashSet<>(getPopulationSize());
+        if (getSelectionFunction() != null) {
+            newGeneration.addAll(getSelectionFunction().evaluate(getPopulation()));
+        }
+        while (newGeneration.size() < getPopulationSize()) {
+            List<T> parents = getSelectionFunctionToParents().evaluate(getPopulation());
+            @SuppressWarnings("unchecked")
+            T[] offspring = getCrossoverFunction().evaluate(parents.get(0), parents.get(1));
+            for (T genome : offspring) {
+                getMutationFunction().evaluate(genome);
+            }
+            if (getGenomeFilter() != null) {
+                for (T genome : offspring) {
+                    if (getGenomeFilter().accept(genome)) {
+                        newGeneration.add(genome);
+                    }
+                }
+            } else {
+                Collections.addAll(newGeneration, offspring);
+            }
+        }
+        setPopulation(new ArrayList<>(newGeneration));
+    }
+
+    /**
+     * Gets the instance of crossover function.
+     *
+     * @return the crossover function.
+     */
+    public CrossoverFunction<T> getCrossoverFunction() {
+        return crossoverFunction;
+    }
+
+    /**
+     * Gets the instance of mutation function.
+     *
+     * @return the mutation function.
+     */
+    public MutationFunction<T> getMutationFunction() {
+        return mutationFunction;
+    }
+
+    /**
+     * Gets the instance of selection function.
+     *
+     * @return the selection function.
+     */
+    public SelectionFunction<T> getSelectionFunction() {
+        return selectionFunction;
+    }
+
+
+    /**
+     * Gets the selection function used to select the parents and use them in the crossing process.
+     * By default is an instance of {@link crow.javartint.gea.function.selection.RandomSelectionFunction}.
+     *
+     * @return an instance of {@link crow.javartint.gea.function.selection.SelectionFunction}
+     */
+    public SelectionFunction<T> getSelectionFunctionToParents() {
+        return selectionFunctionToParents;
+    }
+
+    /**
+     * Sets the selection function used to select the parents in the crossing process.
+     *
+     * @param selectionFunctionToParents selection function instance.
+     */
+    public void setSelectionFunctionToParents(SelectionFunction<T> selectionFunctionToParents) {
+        this.selectionFunctionToParents = selectionFunctionToParents;
+    }
+}
